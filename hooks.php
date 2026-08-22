9f87f55f8e27d65f0543a69d868bb5f3032ddebc
MD Samsuzzaman Siyam <samsusiyam@gmail.com>
2026-08-23 00:45:51 +0600

    fix: preserve WHMCS constant in Yakpro-Po obfuscation

--- hooks.php
+++ hooks.php
@@ -1,8 +1,8 @@
 <?php
 /*   __________________________________________________
     |  Obfuscated by YAK Pro - Php Obfuscator  3.0.0   |
-    |              on 2026-08-22 20:36:59              |
+    |              on 2026-08-22 20:45:41              |
     |    GitHub: https://github.com/pk-fr/yakpro-po    |
     |__________________________________________________|
 */
- goto XKeQZ; OrDpL: w21BG: goto dS2g7; jEhkD: die("\124\x68\x69\163\40\x66\x69\x6c\x65\x20\143\x61\156\156\157\164\40\142\145\x20\x61\143\x63\145\163\x73\x65\x64\40\x64\x69\162\145\143\x74\154\x79"); goto OrDpL; XKeQZ: if (defined("\x71\63\x6a\x61\160")) { goto w21BG; } goto jEhkD; dS2g7: add_hook("\101\144\x6d\151\x6e\101\162\145\141\120\x61\147\145", 1, function ($SsCB4) { goto BbEuB; BbEuB: if (!(isset($SsCB4["\146\x69\154\x65\x6e\141\x6d\145"]) && $SsCB4["\146\x69\x6c\145\x6e\141\x6d\x65"] === "\x61\x64\x64\x6f\156\x6d\157\x64\x75\x6c\x65\163" && isset($_GET["\x6d\x6f\x64\165\154\x65"]) && $_GET["\x6d\x6f\144\x75\x6c\x65"] === "\142\x61\164\143\150\137\144\x65\x6c\x65\164\x65\x5f\x63\x6c\x69\x65\x6e\164\x73")) { goto JMZgh; } goto duct9; duct9: include_once __DIR__ . "\x2f\x63\x6c\151\145\156\164\x5f\144\145\154\145\164\x65\x2e\160\150\x70"; goto DufE9; DufE9: JMZgh: goto aVi1Z; aVi1Z: });
+ goto gQMR5; JeXav: ggBvv: goto pSzLJ; gQMR5: if (defined("\x57\x48\115\103\123")) { goto ggBvv; } goto GiRQm; GiRQm: die("\x54\150\151\x73\x20\146\151\x6c\145\40\x63\x61\156\156\157\164\40\x62\145\40\141\143\143\145\163\x73\145\144\40\x64\x69\162\x65\x63\164\x6c\171"); goto JeXav; pSzLJ: add_hook("\x41\144\155\151\x6e\101\162\x65\x61\x50\x61\x67\x65", 1, function ($pqA7T) { goto Uk3cg; Uk3cg: if (!(isset($pqA7T["\x66\151\154\x65\x6e\x61\155\145"]) && $pqA7T["\146\151\x6c\145\156\141\155\x65"] === "\141\144\144\x6f\156\x6d\157\144\165\154\145\x73" && isset($_GET["\x6d\157\144\x75\x6c\145"]) && $_GET["\155\157\x64\165\x6c\145"] === "\142\x61\164\x63\150\137\144\145\154\145\x74\145\137\x63\154\151\145\x6e\x74\x73")) { goto eil72; } goto Tjtio; Tjtio: include_once __DIR__ . "\57\x63\x6c\151\145\156\x74\137\x64\145\154\145\164\x65\56\160\150\x70"; goto wMEP3; wMEP3: eil72: goto Ve9ra; Ve9ra: });
